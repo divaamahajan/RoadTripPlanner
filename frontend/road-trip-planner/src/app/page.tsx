d084a913a7c01f99d5601bab8b7a6ee5cd0a43b6
Aryan Bagade <aryanbagade@Aryans-MacBook-Pro.local>
2023-11-11 22:01:58 -0800

    Final Changes

--- frontend/road-trip-planner/src/app/page.tsx
+++ frontend/road-trip-planner/src/app/page.tsx
@@ -1,11 +1,11 @@
 import UserLocation from "../components/userLocation";
+import Map from "../components/Map";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <UserLocation />
+        {/* <UserLocation /> */}
+        <Map />
       </div>
-    </main>
   );
 }
