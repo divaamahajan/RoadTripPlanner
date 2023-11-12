8d3662b59f5231b4effd5522c01fb282b1d74e6b
Rohan Lagare <42294538+RohanL1@users.noreply.github.com>
2023-11-11 19:54:56 -0800

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,42 +1,58 @@
 from flask import Flask, jsonify, request
+import requests
+import json, xmltodict
+from math import cos, asin, sqrt, pi
+
+def distance(lat1, lon1, lat2, lon2):
+    r = 3958.756 # km
+    p = pi / 180
+
+    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
+    return 2 * r * asin(sqrt(a))
 
 app = Flask(__name__)
 
-input = {
-    'start' : ['lat', 'long' ],
-    'end' : ['lat', 'long' ],
-    'stop_time' : 60  # in mins
+
+TRIP = {
+    'start' : '37.770581,-122.442550',
+    'dest' : '37.765297,-122.442527',
+    'stopAfter' : 15  # in mins
 }
 
-TRIP = {}
 
-@app.route('/input', methods=['POST'])
-def add_route():
+app_id = ""
+hash_token = ""
+inrix_token=""
+PROXY_SERVER = "http://127.0.0.1:8000/"
+
+@app.route('/set_trip', methods=['POST'])
+def set_trip():
     try:
         data = request.get_json()
 
         # Validate the input
-        if 'start' not in data or 'end' not in data or 'stop_time' not in data:
-            return jsonify({'error': 'Invalid input. Please provide start, end, and stop_time'}), 400
+        if 'start' not in data or 'dest' not in data or 'stopAfter' not in data:
+            return jsonify({'error': 'Invalid input. Please provide start, dest, and stopAfter'}), 400
 
         start = data['start']
-        end = data['end']
-        stop_time = data['stop_time']
+        dest = data['dest']
+        stopAfter = data['stopAfter']
 
         # Validate the coordinates
-        if len(start) != 2 or len(end) != 2:
-            return jsonify({'error': 'Invalid coordinates. Please provide lat and long for start and end'}), 400
+        if len(start) != 2 or len(dest) != 2:
+            return jsonify({'error': 'Invalid coordinates. Please provide lat and long for start and dest'}), 400
 
         # Create a route dictionary
+        global TRIP
         TRIP = {
             'start': {'lat': start[0], 'long': start[1]},
-            'end': {'lat': end[0], 'long': end[1]},
-            'stop_time': stop_time
+            'dest': {'lat': dest[0], 'long': dest[1]},
+            'stopAfter': stopAfter
         }
         
         print(TRIP)
         
-        return jsonify({'message': 'Route added successfully'}), 201
+        return jsonify({'message': TRIP }), 201
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -45,5 +61,35 @@ def add_route():
 def test():
     return jsonify({'data': 'HELLO '})
 
+
+def refresh_tok():
+    response = requests.get(PROXY_SERVER+"/gettoken")
+
+    rjson=response.json()
+    global inrix_token 
+    inrix_token = rjson['result']['token']
+
+
+def get_route_points_and_avg_speed(start, dest):
+    refresh_tok()
+    #testing findRoute and getRoute
+    response = requests.get(PROXY_SERVER+"/getroute",headers={'Authorization': inrix_token, 'start' : start, 'dest' : dest})
+    path_points = response.json()["result"]["trip"]["routes"][0]["points"]["coordinates"]
+    avg_speed = response.json()["result"]["trip"]["routes"][0]["averageSpeed"]
+    print(path_points)
+    print(avg_speed)
+    return path_points, avg_speed
+
+@app.route('/stops', methods=['GET'])
+def get_stops():
+    data, x= get_route_points_and_avg_speed(TRIP['start'], TRIP['dest'])
+    return jsonify({'data': data})
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=443)
+    get_route_points_and_avg_speed(TRIP['start'], TRIP['dest'])
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
+    
+
+
