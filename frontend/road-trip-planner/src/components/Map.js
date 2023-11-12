c59020e96aacf8b5ee37bc8391c74d261d014453
Divya <dm.divya.mahajan@gmail.com>
2023-11-11 22:51:56 -0800

    merged code

--- frontend/road-trip-planner/src/components/Map.js
+++ frontend/road-trip-planner/src/components/Map.js
@@ -12,7 +12,8 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { FaLocationArrow, FaTimes } from "react-icons/fa";
-
+import axios from "axios";
+import StopLocationTable from "./StopLocationTable";
 import {
   useJsApiLoader,
   GoogleMap,
@@ -30,7 +31,12 @@ function Map() {
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
   const [stopAfterMinutes, setStopAfterMinutes] = useState("");
+  const [startLocation, setStartLocation] = useState("");
+  const [destination, setDestination] = useState("");
+  const [stopsFetched, setStopsFetched] = useState(false);
+  const [stopLocation, setStopLocation] = useState({ data: [] });
 
+  const [resultObject, setResultObject] = useState({});
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef();
   /** @type React.MutableRefObject<HTMLInputElement> */
@@ -43,6 +49,45 @@ function Map() {
   if (!isLoaded) {
     return <SkeletonText />;
   }
+  const geocodeLocation = async (location) => {
+    const response = await axios.get(
+      `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
+        location
+      )}&format=json`
+    );
+
+    return response.data && response.data.length > 0 ? response.data[0] : null;
+  };
+  const calculateCoordinates = async () => {
+    let startLat, startLon, destLat, destLon;
+
+    const startResponse = await geocodeLocation(startLocation);
+    const destResponse = await geocodeLocation(destination);
+
+    if (startResponse) {
+      ({ lat: startLat, lon: startLon } = startResponse);
+      console.log("Start Location Coordinates:", [startLat, startLon]);
+    }
+
+    if (destResponse) {
+      ({ lat: destLat, lon: destLon } = destResponse);
+      console.log("Destination Coordinates:", [destLat, destLon]);
+
+      const resultObject = {
+        start: [parseFloat(startLat), parseFloat(startLon)],
+        dest: [parseFloat(destLat), parseFloat(destLon)],
+        stopAfter: stopAfterMinutes,
+        // },
+      };
+
+      console.log("Result Object:", resultObject);
+      setResultObject(resultObject);
+
+      return resultObject;
+    }
+
+    return null;
+  };
 
   async function calculateRoute() {
     if (originRef.current.value === "" || destinationRef.current.value === "") {
@@ -59,7 +104,35 @@ function Map() {
     setDirectionsResponse(results);
     setDistance(results.routes[0].legs[0].distance.text);
     setDuration(results.routes[0].legs[0].duration.text);
+
+    try {
+      const resultObject = await calculateCoordinates();
+      console.log("resultObject", resultObject);
+
+      if (resultObject) {
+        const apiUrl = "/api/set_trip";
+        console.log("Posting result object to", apiUrl);
+        await axios.post(apiUrl, resultObject);
+        console.log("Result object posted successfully!");
+
+        console.log("Getting stops");
+        const response = await axios.get("/api/stops");
+        console.log("Stops received response.data", response.data);
+        setStopLocation(response.data);
+        setStopsFetched(true);
+      }
+    } catch (error) {
+      handleAxiosError(error);
+    }
   }
+
+  const handleAxiosError = (error) => {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios Error:", error.message);
+    } else {
+      console.error("Error:", error);
+    }
+  };
 
   function clearRoute() {
     setDirectionsResponse(null);
@@ -111,10 +184,22 @@ function Map() {
       >
         <HStack spacing={4}>
           <Autocomplete>
-            <Input type="text" placeholder="Origin" ref={originRef} />
+            <Input
+              type="text"
+              placeholder="Origin"
+              value={startLocation}
+              onChange={(e) => setStartLocation(e.target.value)}
+              ref={originRef}
+            />
           </Autocomplete>
           <Autocomplete>
-            <Input type="text" placeholder="Destination" ref={destinationRef} />
+            <Input
+              type="text"
+              placeholder="Destination"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              ref={destinationRef}
+            />
           </Autocomplete>
           <Input
             type="number"
@@ -144,6 +229,7 @@ function Map() {
             onClick={() => map.panTo(center)}
           />
         </HStack>
+        {/* <StopLocationTable stopLocations={stopLocation.data} /> */}
       </Box>
     </Flex>
   );
