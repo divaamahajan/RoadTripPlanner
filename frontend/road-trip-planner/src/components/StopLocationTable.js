1ebef22dbcffb83c993150fc5902088b786eb130
Divya <dm.divya.mahajan@gmail.com>
2023-11-11 20:03:21 -0800

    formatting

--- frontend/road-trip-planner/src/components/StopLocationTable.js
+++ frontend/road-trip-planner/src/components/StopLocationTable.js
@@ -1,7 +1,15 @@
 // StopLocationTable.js
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUtensils, faCoffee, faGlassCheers, faHamburger, faPizzaSlice, faDumbbell, faGasPump } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUtensils,
+  faCoffee,
+  faGlassCheers,
+  faHamburger,
+  faPizzaSlice,
+  faDumbbell,
+  faGasPump,
+} from "@fortawesome/free-solid-svg-icons";
 
 const getIcon = (type) => {
   switch (type) {
@@ -24,31 +32,57 @@ const getIcon = (type) => {
   }
 };
 const StopLocationTable = ({ stopLocations }) => {
+  // Sort the stopLocations by added time
+  const sortedStopLocations = stopLocations.sort(
+    (a, b) => a.addedTime - b.addedTime
+  );
+
   return (
     <div style={{ maxHeight: "300px", overflowY: "auto" }}>
       <h2>Stop Locations</h2>
-      <table style={{ width: "100%", borderCollapse: "collapse" }}>
+      <table
+        style={{
+          width: "100%",
+          borderCollapse: "collapse",
+          border: "1px solid #ddd",
+        }}
+      >
         <thead>
           <tr>
-            <th>Type</th>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Parking Spot count</th>
-            <th>Added Time (minutes)</th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>Type</th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>Name</th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Address
+            </th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Parking Spot count
+            </th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Added Time (minutes)
+            </th>
           </tr>
         </thead>
         <tbody>
-          {stopLocations.map((location) => (
-            <tr key={location.id}>
-              <td>
+          {sortedStopLocations.map((location) => (
+            <tr key={location.id} style={{ borderBottom: "1px solid #ddd" }}>
+              <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
                 {getIcon(location.type) && (
-                  <FontAwesomeIcon icon={getIcon(location.type)} style={{ marginRight: "5px" }} />
+                  <FontAwesomeIcon
+                    icon={getIcon(location.type)}
+                    style={{ marginRight: "5px" }}
+                  />
                 )}
               </td>
-              <td>{location.name}</td>
-              <td>{location.address}</td>
-              <td>{location.parkCount} slots</td>
-              <td>{location.addedTime}</td>
+              <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
+                {location.name}
+              </td>
+              <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
+                {location.address}
+              </td>
+              <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
+                {location.parkCount} slots
+              </td>
+              <td style={{ padding: "8px" }}>{location.addedTime}</td>
             </tr>
           ))}
         </tbody>
