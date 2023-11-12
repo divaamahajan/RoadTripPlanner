81faf079ab146c8585d5d08d07a6e639806a67cd
Divya <dm.divya.mahajan@gmail.com>
2023-11-12 06:47:45 -0800

    Stop location table

--- frontend/road-trip-planner/src/components/StopLocationTable.js
+++ frontend/road-trip-planner/src/components/StopLocationTable.js
@@ -1,38 +1,35 @@
-// StopLocationTable.js
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUtensils,
-  faCoffee,
-  faGlassCheers,
-  faHamburger,
-  faPizzaSlice,
-  faDumbbell,
-  faGasPump,
-} from "@fortawesome/free-solid-svg-icons";
+import { faUtensils, faGasPump } from "@fortawesome/free-solid-svg-icons";
 
 const getIcon = (type) => {
   switch (type) {
     case "restro":
       return faUtensils;
-    case "cafe":
-      return faCoffee;
-    case "bar":
-      return faGlassCheers;
-    case "fast food":
-      return faHamburger;
-    case "pizza":
-      return faPizzaSlice;
-    case "diner":
-      return faDumbbell;
-    case "gas station":
+    case "parking":
       return faGasPump;
     default:
       return null;
   }
 };
-const StopLocationTable = ({ stopLocations }) => {
-  const [selectedRestro, setSelectedRestro] = useState(null);
+
+const StopLocationTable = ({ stopLocations, onAddStops }) => {
+  const [selectedRestros, setSelectedRestros] = useState({});
+
+  const handleRestroSelection = (stopId, restroId) => {
+    setSelectedRestros((prevSelectedRestros) => ({
+      ...prevSelectedRestros,
+      [stopId]: restroId,
+    }));
+  };
+
+  const selectedRows = [];
+
+  const handleAddStops = () => {
+    // Send the list of selected rows back to the parent component
+    onAddStops(selectedRows);
+  };
+
   return (
     <div style={{ maxHeight: "300px", overflowY: "auto" }}>
       <h2>Stop Locations</h2>
@@ -48,61 +45,73 @@ const StopLocationTable = ({ stopLocations }) => {
           >
             <thead>
               <tr>
-                <th style={{ border: "1px solid #ddd", padding: "8px" }}>Type</th>
-                <th style={{ border: "1px solid #ddd", padding: "8px" }}>Name</th>
-                <th style={{ border: "1px solid #ddd", padding: "8px" }}>Address</th>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  Type
+                </th>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  Name
+                </th>
+                <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  Address
+                </th>
               </tr>
             </thead>
             <tbody>
-              {/* Display parking information */}
-              {/* {stop.parking.map((park) => (
-                <tr key={park.id} style={{ borderBottom: "1px solid #ddd" }}>
-                  <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
-                    <FontAwesomeIcon
-                      icon={faGasPump} // Assuming gas pump icon for parking, change as needed
-                      style={{ marginRight: "5px" }}
-                    />
-                  </td>
-                  <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
-                    {park.name}
-                  </td>
-                  <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
-                    {park.location.join(', ')}
-                  </td>
-                </tr>
-              ))} */}
               {/* Display restaurant information with radio buttons */}
-              {stop.restro.map((restro) => (
-                <tr key={restro.id} style={{ borderBottom: "1px solid #ddd" }}>
-                  <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
-                    {getIcon("restro") && (
+              {stop.restro.map((restro) => {
+                const isSelected =
+                  selectedRestros[stop.stop_id] === restro.id;
+
+                if (isSelected) {
+                  selectedRows.push({
+                    ...restro,
+                    type: "restro",
+                  });
+                }
+
+                return (
+                  <tr key={restro.id} style={{ borderBottom: "1px solid #ddd" }}>
+                    <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
+                      <input
+                        type="radio"
+                        name={`restro_${stop.stop_id}`}
+                        value={restro.id}
+                        checked={isSelected}
+                        onChange={() =>
+                          handleRestroSelection(stop.stop_id, restro.id)
+                        }
+                      />
+                    </td>
+                    <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
                       <FontAwesomeIcon
                         icon={getIcon("restro")}
                         style={{ marginRight: "5px" }}
                       />
-                    )}
-                  </td>
-                  <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
-                    {restro.Name}
-                  </td>
-                  <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
-                    {restro.address}
-                  </td>
-                  <td style={{ padding: "8px" }}>
-                    {/* Radio button for selecting the restaurant */}
-                    <input
-                      type="radio"
-                      name={`restro_${stop.stop_id}`}
-                      value={restro.id}
-                      onChange={() => setSelectedRestro(restro.id)}
-                    />
-                  </td>
-                </tr>
-              ))}
+                    </td>
+                    <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
+                      {restro.Name}
+                    </td>
+                    <td style={{ borderRight: "1px solid #ddd", padding: "8px" }}>
+                      {restro.address}
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
       ))}
+      {/* Display the selected rows */}
+      <div>
+        <h3>Selected Rows:</h3>
+        <ul>
+          {selectedRows.map((row, index) => (
+            <li key={index}>{JSON.stringify(row)}</li>
+          ))}
+        </ul>
+      </div>
+      {/* Add Stops button */}
+      <button onClick={handleAddStops}>Add Stops</button>
     </div>
   );
 };
